cc64f4292adb21a38abe4ad6496dfedabe217cf3
dessagrado <dessagrado@gmail.com>
2024-07-10 15:02:23 -0500

    emparejamiento header

--- script/script.js
+++ script/script.js
@@ -7,9 +7,13 @@ window.addEventListener("scroll", function(){
 
 window.addEventListener("scroll", function(){
     var body = document.querySelector("body");
+    var logo = document.querySelector(".hlogo--color")
+    var icon = document.querySelector(".hicon--color");
     body.classList.toggle("otra",window.scrollY>900);
     body.classList.toggle("scroll",window.scrollY>900);
     body.classList.toggle("otra_dos",window.scrollY>3000);
+    logo.classList.toggle("inv",window.scrollY>900);
+    icon.classList.toggle("invert",window.scrollY>900);
 })
 
 
@@ -17,6 +21,7 @@ window.addEventListener("scroll", function(){
     var video = document.querySelector(".inicio__element");
     var texto = document.querySelector(".inicio__banner");
     var sep = document.querySelector(".separador__f");
+
     video.classList.toggle("out__section",window.scrollY>900);
     texto.classList.toggle("out__section",window.scrollY>900);
     sep.classList.toggle("out__section-separador",window.scrollY<3500);
