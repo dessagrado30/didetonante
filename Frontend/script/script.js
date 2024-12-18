0c6fcbedd5c1e0e7e93c2301302eef718dd6496d
dessagrado30 <dessagrado@gmail.com>
2024-12-17 21:44:27 -0500

    avance página InTensión

--- Frontend/script/script.js
+++ Frontend/script/script.js
@@ -1,7 +1,7 @@
 // preloader display none
 const preloader = document.querySelector(".preloader");
 window.addEventListener("load", function(){
-    preloader.classList.add("preloader-button");
+    preloader.classList.add("preloader-hidden");
 })
 function continuar(){
     preloader.classList.add("preloader-hidden");
