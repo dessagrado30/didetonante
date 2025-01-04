e18be757f2548717eb796f22954d3c3ad66cf271
dessagrado30 <dessagrado@gmail.com>
2025-01-04 14:18:32 -0500

    avance menu audiovisual

--- Frontend/script/preload.js
+++ Frontend/script/preload.js
@@ -1,8 +1,8 @@
 // preloader display none
 const preloader = document.querySelector(".preloader");
 window.addEventListener("load", function(){
-    preloader.classList.add("preloader-button");
-    preloader.classList.add("preloader-text");
+    preloader.classList.add("cont-view");
+    
 })
 function continuar(){
     preloader.classList.add("preloader-hidden");
